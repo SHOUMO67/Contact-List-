20c64c2c26d6eaef3b98f67a74d4aa4a2c1bc930
shoumo <shoumodip.das@gmail.com>
2023-08-14 15:52:44 +0530

    1st commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,9 @@
 const mongoose = require('mongoose');
 
 //connect to the database (insteade of localhost i used {127.0.0.1:27017} or {0.0.0.0:27017}) cause localhost gets terminal erroe
-mongoose.connect('mongodb://127.0.0.1:27017/contact_db');
+// mongoose.connect('mongodb://127.0.0.1:27017/contact_db');
+mongoose.connect('mongodb+srv://shoumodip:shoumo@cluster0.scxf24d.mongodb.net/?retryWrites=true&w=majority');
+
 
 //acquire the connection(to check if it's successful)
 const db = mongoose.connection;
